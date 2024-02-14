634fab19f05cd4b27773239a5f62b947bd5f50d2
CakeCoding <160079008+CakeCoding@users.noreply.github.com>
2024-02-14 18:06:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const alternatives = [
+    {text:"", images:"cat.gif"},
+    {text:"Lula, would you like to be my Valentine?", images:"penguin.gif"},
+    {text:"I promise it will be worth it", images:"penguin2.gif"},
+    {text:"Just click yes queen!", images:"hug.gif"},
+    {text:"Don't be scared", images:"omw.gif"}
+];
+const ohyes = {text:"LETS GOOO", images:"catyes.gif"};
+const title = document.querySelector('.title');
+const text = document.querySelector('.text');
+const cat = document.querySelector('.cat');
+const buttons = document.querySelectorAll('.button');
+const errorButton = document.querySelector('.button__error');
+
+let count = 0;
+function updateDisplay(item) {
+    console.log(item);
+    cat.src = item.images;
+    text.innerHTML = item.text;
+}
+
+errorButton.addEventListener('click', () => {
+    count = 0;
+    updateDisplay(alternatives[count]);
+    buttons.forEach(btn => btn.style.display = 'inline-block');
+    errorButton.style.display = 'none';
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.textContent === 'YES'){
+            updateDisplay(ohyes);
+            buttons.forEach(btn => btn.style.display = 'none');
+        }
+        if (button.textContent === 'NO'){
+            count++;
+            if(count < alternatives.length){
+                updateDisplay(alternatives[count]);
+            } else {
+                buttons.forEach(btn => btn.style.display = 'none');
+                errorButton.style.display = 'inline-block';
+            }
+        }
+    });
+});
